e682709ded9e652411578aa61632447759d7d09a
Magdalena Štachová <magdalena.stachova@volny.cz>
2022-05-12 17:37:53 +0200

    changing server.py

--- server.py
+++ server.py
@@ -3,27 +3,6 @@ import csv
 
 app = Flask(__name__)
 
-# $env:FLASK_APP = "server.py"
-# $env:FLASK_ENV = "development"
-# flask run
-#https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
-
-#@app.route('/')
-#def hello_world():
-#    return 'Hello, dfdsf!'
-
-#@app.route('/blog')
-#def blog():
-#    return 'I got nothing'
-
-#@app.route('/')
-#def hello_world():
-#    return render_template("index_old.html")
-
-#@app.route('/<username>')
-#def hello_world(username=None):
-#    return render_template("index_old2.html",name=username)
-
 @app.route('/')
 def my_home():
     return render_template("index.html")
@@ -31,14 +10,6 @@ def my_home():
 @app.route('/<string:page>')
 def load_page(page):
     return render_template(page)
-
-def write_to_txt(new_data):
-    email=new_data["email"]
-    subject=new_data["subject"]
-    message=new_data["message"]
-    data_file = open("database.txt", "a")
-    data_file.write(f"\n{email}\t{subject}\t{message}")
-    data_file.close()
 
 def write_to_csv(new_data):
     email=new_data["email"]
@@ -54,7 +25,6 @@ def submit_form():
     if request.method=="POST":
         try:
             data=request.form.to_dict()
-            #write_to_txt(data)
             write_to_csv(data)
             return redirect("/thanks.html")
         except:
